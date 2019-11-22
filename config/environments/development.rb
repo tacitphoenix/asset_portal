aa378630bb296cb3d7c1fb0064f9a7d46e67b9a1
tacitphoenix <sondiek@gmail.com>
2019-11-22 06:46:27 -0500

    all allow queries to be pretty printed in log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Neo4j config
+  config.neo4j.pretty_logged_cypher_queries = true
 end
