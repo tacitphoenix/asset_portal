6bd3246693162c4c7f9757f45533b63abc5086dc
tacitphoenix <sondiek@gmail.com>
2019-11-22 08:12:18 -0500

    replace isbn for uuid as node's id

--- db/neo4j/migrate/20191122120724_create_book.rb
+++ db/neo4j/migrate/20191122120724_create_book.rb
@@ -1,9 +1,9 @@
 class CreateBook < Neo4j::Migrations::Base
   def up
-    add_constraint :Book, :uuid
+    add_constraint :Book, :isbn
   end
 
   def down
-    drop_constraint :Book, :uuid
+    drop_constraint :Book, :isbn
   end
 end
