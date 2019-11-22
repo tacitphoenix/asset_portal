89ed3830df51a97081991f0b3b1c126a4a08d564
tacitphoenix <sondiek@gmail.com>
2019-11-22 08:34:39 -0500

    a more powerful version of includes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.all.with_associations(:author, :category)
   end
 
   # GET /books/1
