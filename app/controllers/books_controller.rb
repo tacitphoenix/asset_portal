db3ab8dd04e32dc1faef08212ed68d7e76ae6888
tacitphoenix <sondiek@gmail.com>
2019-11-22 09:05:58 -0500

    handle a books many categories in the view and controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:isbn, :title, :year_published, :author, :category)
+      params.require(:book).permit(:isbn, :title, :year_published, :author, :category_ids => [])
     end
 end
