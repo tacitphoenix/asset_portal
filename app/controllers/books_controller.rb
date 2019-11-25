c459b087d929afaa79a488c47d142161a82b4523
tacitphoenix <sondiek@gmail.com>
2019-11-25 16:09:04 -0500

    get form to save category changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:isbn, :title, :year_published, :author, :category_ids => [])
+      params.require(:book).permit(:isbn, :title, :year_published, :author, :categories => [])
     end
 end
